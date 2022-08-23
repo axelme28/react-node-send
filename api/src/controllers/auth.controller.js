ce3ca348efb572fcc1f73cb23cdf111baf21bcc0
Axel Mendez <71779664+axelme28@users.noreply.github.com>
2022-08-22 21:11:21 -0500

    ADD: se agrego el controlador de autenticar

---
+++ api/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+exports.login = async (req, res) => {
+	console.log('login');
+	res.send('login');
+};
+
+exports.register = async (req, res) => {
+	console.log('register');
+	res.send('register');
+};
